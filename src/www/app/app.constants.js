87720693687abf78393a3cc6942d593449df0b08
André Felicio <andree.felicio@gmail.com>
2021-07-03 18:41:41 -0300

    update constants

--- src/www/app/app.constants.js
+++ src/www/app/app.constants.js
@@ -1,3 +1,3 @@
-const SERVIDOR = 'http://localhost:2741/';
+//const SERVIDOR = 'http://localhost:2741/';
 //const SERVIDOR = 'http://192.168.1.110:2741/' //pra testar no celular
-//const SERVIDOR = 'http://musikr-api.tk/';
+const SERVIDOR = 'http://musikr-api.tk/';
